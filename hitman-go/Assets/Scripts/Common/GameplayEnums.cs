e0e926f657d5ced1f2943a3d4991e897d58f4d24
Vinay <vinay@outscal.com>
2019-02-25 18:52:48 +0530

    Enemy scriptable object

--- hitman-go/Assets/Scripts/Common/GameplayEnums.cs
+++ hitman-go/Assets/Scripts/Common/GameplayEnums.cs
@@ -7,4 +7,25 @@ namespace Common
         LEFT,
         RIGHT
     }
+    public enum EnemyType
+    {
+        STATIC,
+        PATROLLING,
+        ROTATING_KNIFE,
+        SNIPER,
+        SHIELDED,
+        DOGS
+    }
+    public enum InteractablePickup
+    {
+        BREIFCASE,
+        STONE,
+        BONE,
+        SNIPER_GUN,
+        DUAL_GUN,
+        TRAP_DOOR,
+        COLOR_KEY,
+        AMBUSH_PLANT,
+        GUARD_DISGUISE
+    }
 }
