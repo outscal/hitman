050afb9fa885d9d624d577fc4894e2acd9246bc7
Bhavyam Kr Goyal <bhavyam@outscal.com>
2019-02-27 22:46:40 +0530

    minor changes

--- hitman-go/Assets/Scripts/PathSystem/PathService.cs
+++ hitman-go/Assets/Scripts/PathSystem/PathService.cs
@@ -8,16 +8,15 @@ namespace PathSystem
 {
     public class PathService : IPathService
     {
-
         List<int> shortestPath;
         GameObject line;
         NodeControllerView nodeprefab, targetNode;
-                 [SerializeField] List<Node> graph = new List<Node>();
+        [SerializeField] List<Node> graph = new List<Node>();
         public PathService(ScriptableGraph _Graph)
         {
-			nodeprefab = _Graph.nodeprefab;
+            nodeprefab = _Graph.nodeprefab;
             targetNode = _Graph.targetNode;
-            line = _Graph.line;            DrawGraph(_Graph);
+            line = _Graph.line; DrawGraph(_Graph);
             Debug.Log("path created");
         }
         public void DrawGraph(ScriptableGraph Graph)
@@ -29,14 +28,15 @@ namespace PathSystem
                 node.connections = Graph.Graph[i].GetConnections();
                 graph.Add(node);
                 nodeprefab.SetNodeID(i);
-				if (graph[i].node.property==NodeProperty.TARGETNODE)
+                if (graph[i].node.property == NodeProperty.TARGETNODE)
                 {
                     GameObject.Instantiate(targetNode.gameObject, new Vector3(node.node.nodePosition.x, node.node.nodePosition.y - 0.195f, node.node.nodePosition.z), Quaternion.identity);
                 }
                 else
                 {
                     GameObject.Instantiate(nodeprefab.gameObject, new Vector3(node.node.nodePosition.x, node.node.nodePosition.y - 0.195f, node.node.nodePosition.z), Quaternion.identity);
-                }                if (node.connections[0] != -1)
+                }
+                if (node.connections[0] != -1)
                 {
                     GameObject.Instantiate(line, new Vector3(node.node.nodePosition.x, node.node.nodePosition.y - 0.195f, node.node.nodePosition.z - 2.5f), Quaternion.Euler(new Vector3(0, 90, 0)));
                 }
@@ -101,10 +101,10 @@ namespace PathSystem
 
         public List<int> GetPickupSpawnLocation(InteractablePickup type)
         {
-             List<int>pickableNodeList = new List<int>();
+            List<int> pickableNodeList = new List<int>();
             for (int i = 0; i < graph.Count; i++)
             {
-                if (graph[i].node.spawnPickups==type)
+                if (graph[i].node.spawnPickups == type)
                 {
                     pickableNodeList.Add(graph[i].node.uniqueID);
                 }
@@ -117,7 +117,7 @@ namespace PathSystem
             int playerNode = -1;
             for (int i = 0; i < graph.Count; i++)
             {
-                if (graph[i].node.property==NodeProperty.SPAWNPLAYER)
+                if (graph[i].node.property == NodeProperty.SPAWNPLAYER)
                 {
                     playerNode = graph[i].node.uniqueID;
                     break;
@@ -149,7 +149,7 @@ namespace PathSystem
 
         public bool CheckForTargetNode(int _NodeID)
         {
-            return graph[_NodeID].node.property==NodeProperty.TARGETNODE;
+            return graph[_NodeID].node.property == NodeProperty.TARGETNODE;
         }
     }
 }
