bd481b5a2abfaac99b701bc7ad64cc97b02e0b87
SwapnilOutscal <swapnil@outscal.com>
2019-02-28 17:06:54 +0530

    Added BriefCase Interactable

---
+++ hitman-go/Assets/Scripts/Interactables/BriefCase/BriefCaseController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Common;
+using Zenject;
+
+namespace InteractableSystem
+{
+    public class BriefCaseController : InteractableController
+    {
+        private InteractableManager interactableManager;
+
+        public BriefCaseController(Vector3 nodePos, InteractableManager interactableManager
+                                            , InteractableView briefCasePrefab)
+        {
+            this.interactableManager = interactableManager;
+            GameObject briefCase = GameObject.Instantiate<GameObject>(briefCasePrefab.gameObject);
+            interactableView = briefCase.GetComponent<BriefCaseView>();
+            interactableView.transform.position = nodePos;
+        }
+
+        protected override void OnInitialized()
+        {
+            interactablePickup = InteractablePickup.BREIFCASE;
+        }
+
+        public override void TakeAction(int nodeID)
+        {
+            interactableManager.SendBriefCaseSignal();
+            interactableView.gameObject.SetActive(false);
+        }
+
+
+    }
+}
