dc7f29880ebe5b09a5b6c9900bd1da5144413a1f
SwapnilOutscal <swapnil@outscal.com>
2019-03-04 15:58:53 +0530

    Edited GameInstaller

--- hitman-go/Assets/Scripts/Common/GameInstaller.cs
+++ hitman-go/Assets/Scripts/Common/GameInstaller.cs
@@ -77,6 +77,11 @@ public class GameInstaller : MonoInstaller
             .AsSingle()
             .NonLazy();
 
+        Container.Bind<ICamera>()
+            .To<CameraManager>()
+            .AsSingle()
+            .NonLazy();
+
         Container.BindInterfacesAndSelfTo<GameService>()
             .AsSingle()
             .NonLazy();
