0b387bf5226a4c7f5978c5b8005de2b1b956af7c
Vinay <vinay@outscal.com>
2019-03-04 18:33:50 +0530

    enemyview bug

--- hitman-go/Assets/Scripts/Enemy/Views/SniperEnemyView.cs
+++ hitman-go/Assets/Scripts/Enemy/Views/SniperEnemyView.cs
@@ -71,8 +71,7 @@ namespace Enemy
                 {                   
                     enemyController.KillPlayer();
                 }
-            }
-            
+            }            
         }
     }
 }
